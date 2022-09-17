ccf10da1e48512f45af0f4268058387fce35aa08
FauziSlebew <kontolngentod607@gmail.com>
2022-09-17 15:53:05 +0800

    master

---
+++ plugins/i-donasi.js
@@ -0,0 +1,32 @@
+let handler = async (m, { conn, usedPrefix }) => { 
+      conn.relayMessage(m.chat,  {
+    requestPaymentMessage: {
+      currencyCodeIso4217: 'INR',
+      amount1000: 1339889,
+      requestFrom: m.sender,
+      noteMessage: {
+      extendedTextMessage: {
+      text: `
+╭─「 Donasi • Dana 」
+│ • XL [6287837703726]
+│ • Gopay  [6287837703726]
+│ • Dana  [6287837703726]
+│ • Qris  [Chat OWNER]
+╰────
+╭─「 *NOTE* 」
+│ > Ingin donasi? Wa.me/6287837703726
+│ _Hasil donasi akan digunakan buat sewa_
+│ _atau beli *RDP/VPS* agar bot bisa jalan_
+│ _24jam tanpa kendala_
+╰────
+`,
+      contextInfo: {
+      externalAdReply: {
+      showAdAttribution: true
+      }}}}}}, {})
+}
+handler.help = ['donasi']
+handler.tags = ['about']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
